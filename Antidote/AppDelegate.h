1cdc1eae2d42aa5ee69a14237f58ab5c044d791a
Dmitry Vorobyov <d@dvor.me>
2014-07-18 16:41:50 +0400

    Initial commit

---
+++ Antidote/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Antidote
+//
+//  Created by Dmitry Vorobyov on 18.07.14.
+//  Copyright (c) 2014 dvor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
