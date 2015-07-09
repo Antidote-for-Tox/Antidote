2a5c32d032fe4bdd243e2c3bf9a3a513b01b4f03
Chuong Vu <chuongv@gmail.com>
2015-07-08 22:22:32 -0700

    DialingViewController updates

--- Antidote/DialingCallViewController.m
+++ Antidote/DialingCallViewController.m
@@ -13,16 +13,21 @@
 #import "OCTChat.h"
 #import "AvatarsManager.h"
 #import "OCTSubmanagerCalls.h"
+#import "AppearanceManager.h"
 #import "ActiveCallViewController.h"
 
 static const CGFloat kIndent = 50.0;
 static const CGFloat kAvatarDiameter = 180.0;
+static const CGFloat kLabelFontSize = 16.0;
+static const CGFloat kButtonBorderWidth = 1.5f;
+static const CGFloat kEndCallButtonHeight = 45.0;
 
 @interface DialingCallViewController ()
 
 @property (strong, nonatomic) UIButton *cancelCallButton;
 @property (assign, nonatomic) dispatch_once_t becameActiveToken;
 @property (strong, nonatomic) UIImageView *friendAvatar;
+@property (strong, nonatomic) UILabel *reachingLabel;
 
 @end
 
@@ -49,6 +54,7 @@ static const CGFloat kAvatarDiameter = 180.0;
 
     [self createEndCallButton];
     [self createFriendAvatar];
+    [self createReachingLabel];
     [self installConstraints];
 }
 
@@ -57,9 +63,16 @@ static const CGFloat kAvatarDiameter = 180.0;
 
 - (void)createEndCallButton
 {
-    self.cancelCallButton = [UIButton new];
+    self.cancelCallButton = [UIButton buttonWithType:UIButtonTypeCustom];
     self.cancelCallButton.backgroundColor = [UIColor redColor];
     [self.cancelCallButton addTarget:self action:@selector(endCall) forControlEvents:UIControlEventTouchUpInside];
+    self.cancelCallButton.layer.cornerRadius = kEndCallButtonHeight / 2.0f;
+
+    UIImage *image = [UIImage imageNamed:@"call-accept"];
+    [self.cancelCallButton setImage:image forState:UIControlStateNormal];
+    self.cancelCallButton.layer.borderColor = [UIColor blackColor].CGColor;
+    self.cancelCallButton.layer.borderWidth = kButtonBorderWidth;
+    self.cancelCallButton.tintColor = [UIColor whiteColor];
 
     [self.view addSubview:self.cancelCallButton];
 }
@@ -79,6 +92,16 @@ static const CGFloat kAvatarDiameter = 180.0;
     [self.view addSubview:self.friendAvatar];
 }
 
+- (void)createReachingLabel
+{
+    self.reachingLabel = [UILabel new];
+    self.reachingLabel.font = [[AppContext sharedContext].appearance fontHelveticaNeueWithSize:kLabelFontSize];
+    self.reachingLabel.textColor = [UIColor whiteColor];
+    self.reachingLabel.text = NSLocalizedString(@"reaching...", @"Calls");
+
+    [self.view addSubview:self.reachingLabel];
+}
+
 - (void)installConstraints
 {
     [super installConstraints];
@@ -89,9 +112,15 @@ static const CGFloat kAvatarDiameter = 180.0;
     }];
 
     [self.cancelCallButton makeConstraints:^(MASConstraintMaker *make) {
+        make.height.equalTo(kEndCallButtonHeight);
         make.bottom.equalTo(self.view.bottom).with.offset(-kIndent);
         make.left.equalTo(self.view.left).with.offset(kIndent);
         make.right.equalTo(self.view.right).with.offset(-kIndent);
+    }];
+
+    [self.reachingLabel makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.nameLabel);
+        make.top.equalTo(self.nameLabel.bottom);
     }];
 }
 
