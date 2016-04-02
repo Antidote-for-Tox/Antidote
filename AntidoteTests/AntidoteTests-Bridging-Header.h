5b94024832c2f302754ad8dc7ba854dd4f9d8324
Dmytro Vorobiov <d@dvor.me>
2016-04-02 11:35:10 +0200

    Adding FBSnapshotTestCase

---
+++ AntidoteTests/AntidoteTests-Bridging-Header.h
@@ -0,0 +1,42 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <objcTox/OCTCall.h>
+#import <objcTox/OCTChat.h>
+#import <objcTox/OCTDefaultFileStorage.h>
+#import <objcTox/OCTFriend.h>
+#import <objcTox/OCTFriendRequest.h>
+#import <objcTox/OCTManager.h>
+#import <objcTox/OCTManagerConfiguration.h>
+#import <objcTox/OCTManagerConstants.h>
+#import <objcTox/OCTMessageAbstract.h>
+#import <objcTox/OCTMessageCall.h>
+#import <objcTox/OCTMessageFile.h>
+#import <objcTox/OCTMessageText.h>
+#import <objcTox/OCTSubmanagerBootstrap.h>
+#import <objcTox/OCTSubmanagerCalls.h>
+#import <objcTox/OCTSubmanagerCallsDelegate.h>
+#import <objcTox/OCTSubmanagerChats.h>
+#import <objcTox/OCTSubmanagerFiles.h>
+#import <objcTox/OCTSubmanagerFilesProgressSubscriber.h>
+#import <objcTox/OCTSubmanagerFriends.h>
+#import <objcTox/OCTSubmanagerObjects.h>
+#import <objcTox/OCTSubmanagerUser.h>
+#import <objcTox/OCTTox.h>
+#import <objcTox/OCTToxConstants.h>
+#import <objcTox/OCTView.h>
+#import <objcTox/RBQFetchedResultsController.h>
+
+#undef LOG_INFO
+#undef LOG_DEBUG
+#import "DDLog.h"
+#import "DDASLLogger.h"
+#import "DDTTYLogger.h"
+
+#import <SDCAlertView/SDCAlertController.h>
+#import <UITextView+Placeholder/UITextView+Placeholder.h>
+
+#import "ExceptionHandling.h"
+
+#import "FBSnapshotTestCase.h"
