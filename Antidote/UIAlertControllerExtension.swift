6b9c1707a62672d0f7b30be336d64a0cc2bafcda
Dmytro Vorobiov <d@dvor.me>
2016-07-24 23:30:42 +0200

    UIAlertControllerExtension: fixing issue with presenting alert from modal controllers

--- Antidote/UIAlertControllerExtension.swift
+++ Antidote/UIAlertControllerExtension.swift
@@ -28,11 +28,40 @@ extension UIAlertController {
             alert.addAction(UIAlertAction(title: String(localized: "error_ok_button"), style: .Cancel, handler: nil))
         }
 
-        let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
-        guard let root = appDelegate?.window?.rootViewController else {
+        guard let visible = visibleViewController() else {
             return
         }
 
-        root.presentViewController(alert, animated: true, completion: nil)
+        visible.presentViewController(alert, animated: true, completion: nil)
+    }
+
+    private class func visibleViewController(rootViewController: UIViewController? = nil) -> UIViewController? {
+        var root: UIViewController
+
+        if let rootViewController = rootViewController {
+            root = rootViewController
+        }
+        else {
+            let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
+
+            guard let controller = appDelegate?.window?.rootViewController else {
+                return nil
+            }
+
+            root = controller
+        }
+
+        guard let presented = root.presentedViewController else {
+            return root
+        }
+
+        if let navigation = presented as? UINavigationController {
+            return visibleViewController(navigation.topViewController)
+        }
+        if let tabBar = presented as? UITabBarController {
+            return visibleViewController(tabBar.selectedViewController)
+        }
+
+        return presented
     }
 }
