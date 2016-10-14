26d6fabff52d23d841dbe1ccfb734b9b8fd09b09
Dmytro Vorobiov <d@dvor.me>
2016-10-14 17:47:23 +0200

    Changing license to MPL 2.0

--- Antidote/AvatarManager.swift
+++ Antidote/AvatarManager.swift
@@ -1,10 +1,6 @@
-//
-//  AvatarManager.swift
-//  Antidote
-//
-//  Created by Dmytro Vorobiov on 07/12/15.
-//  Copyright © 2015 dvor. All rights reserved.
-//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import Foundation
 
--- Antidote/KeychainManager.swift
+++ Antidote/KeychainManager.swift
@@ -1,10 +1,6 @@
-//
-//  KeychainManager.swift
-//  Antidote
-//
-//  Created by Dmytro Vorobiov on 13/08/16.
-//  Copyright © 2016 dvor. All rights reserved.
-//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import Foundation
 
